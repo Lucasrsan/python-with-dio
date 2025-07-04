ec093633153da40b580555866a51996027fae799
Lucasrsan <lucasrodriguespsantos@gmail.com>
2025-07-04 14:58:11 -0300

    adiciona exercicios do modulo de estrutura de dados

---
+++ cursos/01 - Estrutura de Dados/01 - Listas/00_declarando_listas.py
@@ -0,0 +1,15 @@
+# As listas são mutáveis e na sintaxe utilizam-se []
+frutas = ["laranja", "maca", "uva"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 4200000, 2020, 2900, "São Paulo", True]
+print(carro)
