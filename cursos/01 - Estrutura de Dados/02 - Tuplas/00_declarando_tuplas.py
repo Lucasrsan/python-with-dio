434ad31678f658515be5a78e2b72bb7d21be9f3c
Lucas Santos <lucasrodriguespsantos@gmail.com>
2025-07-10 17:50:17 -0300

    modulo de tuplas revisado

--- cursos/01 - Estrutura de Dados/02 - Tuplas/00_declarando_tuplas.py
+++ cursos/01 - Estrutura de Dados/02 - Tuplas/00_declarando_tuplas.py
@@ -3,13 +3,13 @@ frutas = (
     "pera",
     "uva",
 )
-print(frutas)
+print(frutas) # laranja, pera, uva
 
-letras = tuple("python")
-print(letras)
+letras = tuple("python") #separa as letras
+print(letras) # p, y, t, h, o, n
 
 numeros = tuple([1, 2, 3, 4])
-print(numeros)
+print(numeros) # 1,2,3,4
 
 pais = ("Brasil",)
-print(pais)
+print(pais) # Brasil
--- cursos/01 - Estrutura de Dados/02 - Tuplas/03_indices_negativos.py
+++ cursos/01 - Estrutura de Dados/02 - Tuplas/03_indices_negativos.py
@@ -1,9 +1,10 @@
-frutas = (
-    "maçã",
-    "laranja",
-    "uva",
-    "pera",
+estados = (
+    "São Paulo",
+    "Rio de Janeiro",
+    "Santa Catarina",
+    "Amazonas"
 )
 
-print(frutas[-1])  # pera
-print(frutas[-3])  # laranja
+print(estados[1]) #Rio de Janeiro
+print(estados[1:]) #Retorna tudo a partir do indice 1
+print(estados[-1]) #Retorna o ultimo elemento
